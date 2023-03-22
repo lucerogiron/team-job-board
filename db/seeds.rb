22f303816f6825113c13878f78dfde29e1e08b16
“lukenilsson” <luke.nilsson@gmail.com>
2023-03-22 15:05:04 -0400

    Companies in Seeds, added bcrypt gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# company = Company.create(
+#   name: "Taco Bell",
+#   logo: "https://1000logos.net/wp-content/uploads/2017/06/Taco-Bell-Logo-500x313.png",
+#   description: "Taco Bell is an American-based chain of fast food restaurants founded in 1962 by Glen Bell (1923–2010) in Downey, California.",
+# )
+# company = Company.create(
+#   name: "Target",
+#   logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Target_%282018%29.svg/800px-Target_%282018%29.svg.png",
+#   description: "An American retail corporation headquartered in Minneapolis, Minnesota. It is the eighth largest retailer in the United States, and a component of the S&P 500 Index. The company is one of the largest American-owned private employers in the United States.",
+# )
+# company = Company.create(
+#   name: "Ritz-Carlton Hotel Company",
+#   logo: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a0/RitzCarlton.svg/1024px-RitzCarlton.svg.png",
+#   description: "The Ritz-Carlton is an American multinational company that operates the luxury hotel chain known as The Ritz-Carlton. The company has 108 luxury hotels and resorts in 30 countries and territories with 29,158 rooms, in addition to 46 hotels with 8,755 rooms planned for the future",
+# )
