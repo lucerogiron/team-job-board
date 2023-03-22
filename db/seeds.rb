a0dbf672d6605772b010bdfd43e3c8665ce9cb2f
lucerogiron <gironlucero98@gmail.com>
2023-03-22 13:38:26 -0700

    added jwt gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
 # User.create(name: "Luke", email: "forceBwithU@example.com", password: "password")
 # User.create(name: "Neil", email: "EllisonPower@example.com", password: "password2")
 # User.create(name: "Lucero", email: "CodingWizard@example.com", password: "password3")
