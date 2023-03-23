cbd4cf3dec6d76f84e38b33532d75f842d29f7be
“lukenilsson” <luke.nilsson@gmail.com>
2023-03-23 12:53:42 -0400

    hey

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,4 @@ Rails.application.routes.draw do
   resources :users
   resources :jobs
   resources :companies
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  #vroom vroom car noises
 end
