fb1763ba1a7d2250b3c4941a9407e8d638fb2f90
NeilEllison <ellison.neil1@gmail.com>
2023-03-23 11:51:16 -0700

    added login page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users
   resources :jobs
   resources :companies
+  resources :sessions, only: [:new, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
