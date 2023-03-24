1a57d07a2ab83393804114c5a1281bcd077736d0
lucerogiron <gironlucero98@gmail.com>
2023-03-24 09:29:44 -0700

    erased jwt from app

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,36 +1,22 @@
 class SessionsController < ApplicationController
-  skip_before_action :require_login, only: [:create, :new]
+  # skip_before_action :require_login, only: [:create, :new]
 
   def new
     render template: "sessions/new"
   end
 
   def create
-    session_params = params.permit(:email, :password)
-    user = User.find_by(email: session_params[:email])
+    user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      jwt = JWT.encode(
-        {
-          user_id: user.id, # the data to encode
-          exp: 24.hours.from_now.to_i, # the expiration time
-        },
-        Rails.application.secrets.fetch(:secret_key_base), # the secret key
-        "HS256" # the encryption algorithm
-      )
-      redirect_to user
+      session[:user_id] = user.id
+      redirect_to "/"
     else
-      # render json: {}, status: :unauthorized
-      flash[:notice] = "Login is invalid!"
-      redirect_to new_session_path
+      redirect_to "/login"
     end
-  end
-
-  def show
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = "You have been signed out!"
-    redirect_to new_session_path
+    redirect_to "/login"
   end
 end
